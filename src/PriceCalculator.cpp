cbbc2c2db3a6973d65e21e1252ddbe703a70b48c
JyamlamLaiHust <l2969069613@gmail.com>
2023-11-28 21:07:20 +0800

    6个需求实现代码全部修改为函数式编程实现，测试用例全部通过

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -23,14 +23,14 @@ double PriceCalculator::AcceptCash(const DiscountType discountType, const double
 
     if (discountMap.empty()) {
         discountMap.emplace(DiscountType::CASH_NORMAL, [](double money) noexcept { return money; });
-        // discountMap.emplace(DiscountType::CASH_PERCENTOFF_10, [](const double money) noexcept { return money * 0.9; });
-        // discountMap.emplace(DiscountType::CASH_PERCENTOFF_20, [](const double money) noexcept { return money * 0.8; });
-        // discountMap.emplace(DiscountType::CASH_PERCENTOFF_30, [](const double money) noexcept { return money * 0.7; });
-        // discountMap.emplace(DiscountType::CASH_BACK, [](const double money) noexcept {
-        //     const double threshold = 100.0;
-        //     const double cashback = 20.0;
-        //     return money - std::floor(money / threshold) * cashback;
-        // });
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_10, [](const double money) noexcept { return money * 0.9; });
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_20, [](const double money) noexcept { return money * 0.8; });
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_30, [](const double money) noexcept { return money * 0.7; });
+        discountMap.emplace(DiscountType::CASH_BACK, [](const double money) noexcept {
+            const double threshold = 100.0;
+            const double cashback = 20.0;
+            return money - std::floor(money / threshold) * cashback;
+        });
     }
 
     return discountMap[discountType](money);
