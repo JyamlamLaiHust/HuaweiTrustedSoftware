71114e2e1f5513803e5a3d2b76b43c4049599ea2
JyamlamLaiHust <l2969069613@gmail.com>
2023-11-28 09:04:48 +0800

    discountMap只生成一次

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -10,15 +10,16 @@ PriceCalculator* PriceCalculator::instance = nullptr;
 
 double PriceCalculator::AcceptCash(const DiscountType discountType, const double money) const noexcept
 {
-    std::unordered_map <DiscountType, std::unique_ptr<Discount>> discountMap;
+    static std::unordered_map <DiscountType, std::unique_ptr<Discount>> discountMap;
     // std::unordered_map <DiscountType, std::function<double(const double)>> discountMap;
-    discountMap.emplace(DiscountType::CASH_NORMAL, std::make_unique<Normal>());
-    // 
-    discountMap.emplace(DiscountType::CASH_PERCENTOFF_10, std::make_unique<CASH_PERCENT_OFF>(0.9));
-    discountMap.emplace(DiscountType::CASH_PERCENTOFF_20, std::make_unique<CASH_PERCENT_OFF>(0.8));
-    discountMap.emplace(DiscountType::CASH_PERCENTOFF_30, std::make_unique<CASH_PERCENT_OFF>(0.7));
-    discountMap.emplace(DiscountType::CASH_BACK, std::make_unique<CashBack>());
-
+    if (discountMap.empty())
+    {
+        discountMap.emplace(DiscountType::CASH_NORMAL, std::make_unique<Normal>());
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_10, std::make_unique<CASH_PERCENT_OFF>(0.9));
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_20, std::make_unique<CASH_PERCENT_OFF>(0.8));
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_30, std::make_unique<CASH_PERCENT_OFF>(0.7));
+        discountMap.emplace(DiscountType::CASH_BACK, std::make_unique<CashBack>());
+    }
     return discountMap.find(discountType)->second->AcceptCash(money);
 }  // namespace PriceCalc
 };
